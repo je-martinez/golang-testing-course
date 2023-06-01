5bb4de7a79d86389a7621863500a9559b7d55d4b
Jose E. Martinez <josemartinezchevez@gmail.com>
2023-06-01 11:57:40 -0600

    chore: update golang course

--- webapp/cmd/web/setup_test.go
+++ webapp/cmd/web/setup_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
+	"webapp/pkg/db"
 )
 
 var app Application
@@ -10,5 +12,13 @@ var app Application
 func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 	app.Session = getSession()
+	app.DSN = "host=localhost port=5433 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	app.DB = db.PostgresConn{DB: conn}
 	os.Exit(m.Run())
 }
