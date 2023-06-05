9121acff54323892cbe7a28f100252675bd14352
Jose E. Martinez <josemartinezchevez@gmail.com>
2023-06-05 16:08:15 -0600

    chore: update golang testing course

--- webapp/cmd/api/api-handlers.go
+++ webapp/cmd/api/api-handlers.go
@@ -48,6 +48,18 @@ func (app *Application) authenticate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	http.SetCookie(w, &http.Cookie{
+		Name:     "_Host-refresh-token",
+		Path:     "/",
+		Value:    tokenPairs.RefreshToken,
+		Expires:  time.Now().Add(refreshTokenExpiry),
+		MaxAge:   int(refreshTokenExpiry.Seconds()),
+		SameSite: http.SameSiteStrictMode,
+		Domain:   "localhost",
+		HttpOnly: true,
+		Secure:   true,
+	})
+
 	//send token to user
 	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
